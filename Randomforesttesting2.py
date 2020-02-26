dc2daf6ff82c2491cc110ebcdb40bfc34bf5f105
AGNICODEHUNTERS <61060149+AGNICODEHUNTERS@users.noreply.github.com>
2020-02-26 22:52:37 +0530

    2

--- Randomforesttesting2.py
+++ Randomforesttesting2.py
@@ -6,6 +6,7 @@ from sklearn.tree import DecisionTreeClassifier
 
 X, y = make_blobs(n_samples=10000, n_features=10, centers=100,random_state=0)
 
+
 clf = DecisionTreeClassifier(max_depth=None, min_samples_split=2,random_state=0)
 scores = cross_val_score(clf, X, y, cv=5)
 scores.mean()
