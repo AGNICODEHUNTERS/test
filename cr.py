1d2b612c31ef18aca438d009c3b7e930c7fa87bd
crezix <ireshmadhusankka@gmail.com>
2020-02-26 23:59:24 +0530

    cr

---
+++ cr.py
@@ -0,0 +1,10 @@
+from sklearn.model_selection import cross_val_score
+from sklearn.datasets import make_blobs
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+X, y = make_blobs(n_samples=10000, n_features=10, centers=100,
+    random_state=0)
+
+print(len(X))
