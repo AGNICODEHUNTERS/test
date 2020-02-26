3b09c9bb6e7fe5dd312b3643531db53690d5b6ab
crezix <ireshmadhusankka@gmail.com>
2020-02-26 22:41:49 +0530

    ir

--- Randomforesttesting.py
+++ Randomforesttesting.py
@@ -1,7 +1,11 @@
 from sklearn.ensemble import RandomForestClassifier
-X = [[0, 0], [1, 1]]
-Y = [0, 1]
-clf = RandomForestClassifier(n_estimators=10)
-clf2 = clf.fit(X, Y)
+X = [[0, 0], [1, 1,],[0,1]]
+Y = [0, 1, 1]
+clf = RandomForestClassifier(n_estimators=100,warm_start=True,max_features=None)
+clf = clf.fit(X, Y)
 
-print(clf2)
+P=[[1,0]]
+
+A=clf.predict(P)
+
+print(A)
